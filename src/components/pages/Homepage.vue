2f92cabe94f9bf37b76d2b7d422d660e2b1a9d67
Sebastian Hegardt <se.hegardt@gmail.com>
2020-03-31 18:24:36 +0900

    Fixed line endings and text on home page

--- src/components/pages/Homepage.vue
+++ src/components/pages/Homepage.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-      <p>Hej och välkommen till släktforskningssidan för Hegardt-släkten! Detta är ett projekt som fortsätter på det<a href="/ansedel/">Peter Hegardt
-        gjort med</a><a href="http://www.hegardt.se/disgen/html/"> hegardt.se.</a></p>
+      <p>Hej och välkommen till släktforskningssidan för Hegardt-släkten! Detta är ett projekt som fortsätter på det <router-link :to="{name: 'PersonalFile', params: {}}">Peter
+        Hegardt gjort med </router-link><a href="http://www.hegardt.se/disgen/html/"> hegardt.se.</a></p>
       <p>Jag som har utvecklat den här sidan heter Sebastian Hegardt</p>
   </div>
 </template>
 
 <script>
   export default {
-    name: "Homepage"
+    name: 'Homepage',
   };
 </script>
 
