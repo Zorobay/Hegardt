28c753512ed9ccf976e017597c67cd9ce6e9dd3e
Sebastian Hegardt <se.hegardt@gmail.com>
2025-12-13 17:50:12 +0100

    Fixed fontawesome import issues

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,9 @@ import App from './App.vue';
 import router from './router/router.ts';
 
 import 'bootstrap';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/css/fontawesome.css';
-import '@fortawesome/fontawesome-free/css/solid.css';
-import '@fortawesome/fontawesome-free/css/brands.css';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 /* import the fontawesome core */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 /* import specific icons */
