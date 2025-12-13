dd41d59180c3fbc976f69305b52179eb05d57094
Sebastian Hegardt <se.hegardt@gmail.com>
2025-12-13 16:52:24 +0100

    Added minimization functionality to map controls

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,33 @@
-import './assets/css/main.css'
+import './assets/css/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router/router.ts'
+import App from './App.vue';
+import router from './router/router.ts';
 
-import "bootstrap"
-import "@fortawesome/fontawesome-free/js/fontawesome"
-import "@fortawesome/fontawesome-free/js/regular"
-import "@fortawesome/fontawesome-free/js/solid"
-import "@fortawesome/fontawesome-free/css/fontawesome.css"
-import "@fortawesome/fontawesome-free/css/solid.css"
-import "@fortawesome/fontawesome-free/css/brands.css"
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-const app = createApp(App)
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-app.use(createPinia())
-app.use(router)
+/* import specific icons */
+import { faAnglesUp } from '@fortawesome/free-solid-svg-icons';
 
-app.mount('#app')
+/* add icons to the library */
+library.add(faAnglesUp);
+
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
